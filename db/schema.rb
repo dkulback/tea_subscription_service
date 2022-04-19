3621781341b5f7f1915d3427c91812be31ecc94a
Darren Kulback <dkulback@gmail.com>
2022-04-19 10:25:13 -0600

    Add description to tea

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_19_001241) do
+ActiveRecord::Schema.define(version: 2022_04_19_161939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2022_04_19_001241) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   add_foreign_key "subscriptions", "customers"
