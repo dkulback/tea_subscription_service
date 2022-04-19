29c1fba0bf42a94a717c7c332d576c31109dff6a
Darren Kulback <dkulback@gmail.com>
2022-04-19 16:58:58 -0600

    Update subscription endpoint

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,6 +1,19 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :find_subscription, only: :update
   def create
     subscription = SubscriptionService.create_subscription(params)
     render json: SubscriptionSerializer.subscription(subscription), status: :created
+  end
+
+  def update
+    @subscription.update(status: params[:status])
+
+    render json: SubscriptionSerializer.subscription(@subscription), status: :ok
+  end
+
+  private
+
+  def find_subscription
+    @subscription = Subscription.find(params[:id])
   end
 end
--- spec/requests/subscriptions_spec.rb
+++ spec/requests/subscriptions_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'API Subscriptions' do
   let(:customer) {  create(:customer) }
   let(:tea) { create(:tea) }
   let(:valid_sub) { { customer_id: customer.id, tea_id: tea.id, plan: 'Monthly' } }
+  let(:cancel_sub) { { status: 'cancelled' } }
   describe 'POST api/v1/subscriptions' do
     context 'when body is valid' do
       it 'returns a Subscription json and status 201' do
@@ -43,6 +44,42 @@ RSpec.describe 'API Subscriptions' do
         errors = JSON.parse(response.body, symbolize_names: true)
         expect(response).to have_http_status(422)
         expect(errors[:error]).to match('Validation failed: Customer has already been taken')
+      end
+    end
+  end
+  describe 'PATCH api/v1/subscriptions' do
+    context 'when body is valid' do
+      it 'returns a Subscription json and status 200' do
+        subscription = Subscription.create(customer_id: customer.id,
+                                           tea_id: tea.id,
+                                           title: "#{customer.name}: #{tea.name}",
+                                           price: tea.price,
+                                           plan: 'Monthly')
+        patch api_v1_subscription_path(subscription), headers: headers, params: JSON.generate(cancel_sub)
+
+        sub = JSON.parse(response.body, symbolize_names: true)
+
+        expect(sub).to have_key(:data)
+        expect(sub[:data]).to have_key(:type)
+        expect(sub[:data]).to have_key(:id)
+        expect(sub[:data][:type]).to eq('subscription')
+        expect(sub[:data][:id]).to be_a(Integer)
+
+        expect(sub[:data][:attributes]).to have_key(:customer_id)
+        expect(sub[:data][:attributes]).to have_key(:tea_id)
+        expect(sub[:data][:attributes]).to have_key(:status)
+        expect(sub[:data][:attributes]).to have_key(:price)
+        expect(sub[:data][:attributes]).to have_key(:title)
+        expect(sub[:data][:attributes]).to have_key(:plan)
+
+        expect(sub[:data][:attributes][:customer_id]).to be_a(Integer)
+        expect(sub[:data][:attributes][:tea_id]).to be_a(Integer)
+        expect(sub[:data][:attributes][:price]).to be_a(Float)
+        expect(sub[:data][:attributes][:title]).to be_a(String)
+        expect(sub[:data][:attributes][:plan]).to be_a(String)
+
+        expect(sub[:data][:attributes][:status]).to eq('cancelled')
+        expect(response).to have_http_status(200)
       end
     end
   end
