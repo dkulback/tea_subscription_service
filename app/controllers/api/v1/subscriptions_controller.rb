d887899b611396146bcf394c38f732e2799885fb
Darren Kulback <dkulback@gmail.com>
2022-11-03 18:20:27 -0600

    Add subscription index

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::SubscriptionsController < ApplicationController
   before_action :find_subscription, only: :update
+  def index
+    subscriptions = Subscription.where(customer_id: params[:customer_id])
+    render json: SubscriptionSerializer.all_subscriptions(subscriptions)
+  end
+
   def create
     subscription = SubscriptionService.create_subscription(params)
     render json: SubscriptionSerializer.subscription(subscription), status: :created
